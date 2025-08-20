6f73cc334c5c8cd9970cdf3fdad5f63503b1d1f7
KTH <taehyeonentus@gmail.com>
2025-08-20 21:09:18 +0900

    enable jpa auditing for base entity

--- resourceServer/src/main/java/entus/resourceServer/ResourceServerApplication.java
+++ resourceServer/src/main/java/entus/resourceServer/ResourceServerApplication.java
@@ -2,8 +2,10 @@ package entus.resourceServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ResourceServerApplication {
 
 	public static void main(String[] args) {
