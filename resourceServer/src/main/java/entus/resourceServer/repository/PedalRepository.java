ae86cd1b2627786cc1748066dab09c0bedb826e7
KTH <taehyeonentus@gmail.com>
2025-08-22 21:13:55 +0900

    update pedal repository

--- resourceServer/src/main/java/entus/resourceServer/repository/PedalRepository.java
+++ resourceServer/src/main/java/entus/resourceServer/repository/PedalRepository.java
@@ -1,7 +1,11 @@
 package entus.resourceServer.repository;
 
+import entus.resourceServer.domain.Category;
 import entus.resourceServer.domain.Pedal;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PedalRepository extends JpaRepository<Pedal, Long> {
+    List<Pedal> findByCategory(Category category);
 }
