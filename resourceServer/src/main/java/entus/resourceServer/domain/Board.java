633d9dd26138c2ed73faa0cb1fdd9e7b134f1e68
KTH <taehyeonentus@gmail.com>
2025-09-01 20:18:01 +0900

    add orphan removal tag (pedal list on board)

--- resourceServer/src/main/java/entus/resourceServer/domain/Board.java
+++ resourceServer/src/main/java/entus/resourceServer/domain/Board.java
@@ -21,7 +21,7 @@ public class Board extends BaseEntity {
     @JoinColumn(name = "user_id")
     private User user;
 
-    @OneToMany(mappedBy = "board", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "board", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<BoardPedal> pedals = new ArrayList<>();
 
     private String name;
