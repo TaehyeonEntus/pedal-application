240dca310ed6d322a40d25480743dbc7f4c742e1
KTH <taehyeonentus@gmail.com>
2025-09-17 22:55:33 +0900

    update Page Dto

--- resourceServer/src/main/java/entus/resourceServer/domain/dto/page/BoardDetailPageDto.java
+++ resourceServer/src/main/java/entus/resourceServer/domain/dto/page/BoardDetailPageDto.java
@@ -1,4 +1,13 @@
 package entus.resourceServer.domain.dto.page;
 
+import entus.resourceServer.domain.dto.response.BoardDetailDto;
+import lombok.Data;
+
+@Data
 public class BoardDetailPageDto {
+    BoardDetailDto boardDetail;
+
+    public BoardDetailPageDto(BoardDetailDto boardDetailDto) {
+        this.boardDetail = boardDetailDto;
+    }
 }
