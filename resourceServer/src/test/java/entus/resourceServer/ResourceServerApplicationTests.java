5843b00f45737f0969fdc7e1a585d8a959fc10ff
KTH <taehyeonentus@gmail.com>
2025-09-17 23:27:11 +0900

    update tests

--- resourceServer/src/test/java/entus/resourceServer/ResourceServerApplicationTests.java
+++ resourceServer/src/test/java/entus/resourceServer/ResourceServerApplicationTests.java
@@ -1,11 +1,17 @@
 package entus.resourceServer;
 
+import entus.resourceServer.config.R2Config;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @SpringBootTest
 class ResourceServerApplicationTests {
-
+	@MockitoBean
+	R2Config r2Config;
+	@MockitoBean
+	S3Presigner s3Presigner;
 	@Test
 	void contextLoads() {
 	}
