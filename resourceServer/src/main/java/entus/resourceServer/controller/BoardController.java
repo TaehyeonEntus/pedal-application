19053d05bfb147076a1777aee7bcfcd1198bbcb3
KTH <taehyeonentus@gmail.com>
2025-09-20 05:39:19 +0900

    update request paths on front pages

--- resourceServer/src/main/java/entus/resourceServer/controller/BoardController.java
+++ resourceServer/src/main/java/entus/resourceServer/controller/BoardController.java
@@ -15,7 +15,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.Duration;
-import java.util.Map;
 
 @RequestMapping("/board")
 @RestController
